658cbba22b9f48e151cecb3a98a96aa9e3696d89
Tatyanochka <tanyasmit16@yandex.ru>
2024-03-29 18:10:01 +0300

    new testsSearch

--- src/test/java/TodosTest.java
+++ src/test/java/TodosTest.java
@@ -33,13 +33,28 @@ public class TodosTest {
     @Test
     public void shouldSearchSimple() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(6, "Позвонить пингвину");
+        SimpleTask simpleTask3= new SimpleTask(7, "Позвонить");
         Todos todos = new Todos();
         todos.add(simpleTask);
-        //simpleTask.matches("Позвонить родителям");
-        todos.search("Позвонить родителям");
-
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
 
         Task[] expected = {simpleTask};
+        Task[] actual = todos.search("Позвонить родителям");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldSearchSimpleTwice() {
+        SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(6, "Позвонить пингвину");
+        SimpleTask simpleTask3= new SimpleTask(7, "Позвонить родителям");
+        Todos todos = new Todos();
+        todos.add(simpleTask);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
+
+        Task[] expected = {simpleTask, simpleTask3};
         Task[] actual = todos.search("Позвонить родителям");
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -47,30 +62,62 @@ public class TodosTest {
     @Test
     public void shouldNotSearchSimple() {
         SimpleTask simpleTask = new SimpleTask(5, "Позвонить родителям");
+        SimpleTask simpleTask2 = new SimpleTask(6, "Позвонить попугаю");
+        SimpleTask simpleTask3= new SimpleTask(7, "Позвонить родителям");
         Todos todos = new Todos();
         todos.add(simpleTask);
+        todos.add(simpleTask2);
+        todos.add(simpleTask3);
 
         Task[] expected = {};
         Task[] actual = todos.search("Позвонить пингвину");
         Assertions.assertArrayEquals(expected, actual);
     }
 
-    @Test
+        @Test
     public void shouldSearchEpic() {
-        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
+            String[] subtasks1 = {"Молоко", "Яйца", "Хлеб"};
+            String[] subtasks2 = {"Корова", "Курица", "Пшеница"};
+            String[] subtasks3 = {"Молоко", "Яйца", "Нехлеб"};
+            Epic epic1 = new Epic(55, subtasks1);
+            Epic epic2 = new Epic(66, subtasks2);
+            Epic epic3 = new Epic(77, subtasks3);
+            Todos todos = new Todos();
+            todos.add(epic1);
+            todos.add(epic2);
+            todos.add(epic3);
+        Task[] expected =  {epic1};
+        Task[] actual = todos.search("Хлеб");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void shouldSearchEpicTwice() {
+        String[] subtasks1 = {"Молоко", "Яйца", "Хлеб"};
+        String[] subtasks2 = {"Корова", "Курица", "Пшеница"};
+        String[] subtasks3 = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic1 = new Epic(55, subtasks1);
+        Epic epic2 = new Epic(66, subtasks2);
+        Epic epic3 = new Epic(77, subtasks3);
         Todos todos = new Todos();
-        todos.add(epic);
-        Task[] expected =  {epic};
+        todos.add(epic1);
+        todos.add(epic2);
+        todos.add(epic3);
+        Task[] expected =  {epic1, epic3};
         Task[] actual = todos.search("Хлеб");
         Assertions.assertArrayEquals(expected, actual);
     }
     @Test
     public void shouldNotSearchEpic() {
-        String[] subtasks = {"Молоко", "Яйца", "Хлеб"};
-        Epic epic = new Epic(55, subtasks);
+        String[] subtasks1 = {"Молоко", "Яйца", "Хлеб"};
+        String[] subtasks2 = {"Корова", "Курица", "Пшеница"};
+        String[] subtasks3 = {"Молоко", "Яйца", "Хлеб"};
+        Epic epic1 = new Epic(55, subtasks1);
+        Epic epic2 = new Epic(66, subtasks2);
+        Epic epic3 = new Epic(77, subtasks3);
         Todos todos = new Todos();
-        todos.add(epic);
+        todos.add(epic1);
+        todos.add(epic2);
+        todos.add(epic3);
         Task[] expected = {};
         Task[] actual = todos.search("Паста");
         Assertions.assertArrayEquals(expected, actual);
@@ -84,11 +131,57 @@ public class TodosTest {
                 "Приложение НетоБанка",
                 "Во вторник после обеда"
         );
+        Meeting meeting2 = new Meeting(
+                666,
+                "Выкатка 4й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Meeting meeting3 = new Meeting(
+                777,
+                "Выкатка 5й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
 
         Todos todos = new Todos();
         todos.add(meeting);
+        todos.add(meeting2);
+        todos.add(meeting3);
         todos.search("Выкатка 3й версии приложения");
         Task[] expected = {meeting};
+        Task[] actual = todos.search("Выкатка 3й версии приложения");
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSearchMeetingTwice() {
+        Meeting meeting = new Meeting(
+                555,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Meeting meeting2 = new Meeting(
+                666,
+                "Выкатка 4й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Meeting meeting3 = new Meeting(
+                777,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+
+        Todos todos = new Todos();
+        todos.add(meeting);
+        todos.add(meeting2);
+        todos.add(meeting3);
+
+        todos.search("Выкатка 3й версии приложения");
+        Task[] expected = {meeting, meeting3};
         Task[] actual = todos.search("Выкатка 3й версии приложения");
         Assertions.assertArrayEquals(expected, actual);
     }
@@ -100,9 +193,23 @@ public class TodosTest {
                 "Приложение НетоБанка",
                 "Во вторник после обеда"
         );
+        Meeting meeting2 = new Meeting(
+                666,
+                "Выкатка 4й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
+        Meeting meeting3 = new Meeting(
+                777,
+                "Выкатка 3й версии приложения",
+                "Приложение НетоБанка",
+                "Во вторник после обеда"
+        );
 
         Todos todos = new Todos();
         todos.add(meeting);
+        todos.add(meeting2);
+        todos.add(meeting3);
         Task[] expected = {};
         Task[] actual = todos.search("Что-то там");
         Assertions.assertArrayEquals(expected, actual);
